0bb2fdbb3cdbbc22d54e07e33f58ec5161ac8861
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-03-15 15:53:42 +0200

    Blog controller added

---
+++ OnlineGroceryHub/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineGroceryHub.Core.Contracts;
+
+namespace OnlineGroceryHub.Controllers
+{
+	public class BlogController : BaseController
+	{
+		private readonly IBlogService blogService;
+
+		public BlogController(IBlogService _blogService)
+		{
+			blogService = _blogService;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
