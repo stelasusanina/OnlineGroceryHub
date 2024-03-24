6078a78ccc8b62ec0408010c59980f15119c7636
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-03-24 10:32:27 +0200

    Renamed dto

---
+++ OnlineGroceryHub.Core/Models/Blog/ArticleDTO.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineGroceryHub.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineGroceryHub.Core.Models.Blog
+{
+	public class ArticleDTO
+	{
+		public int Id { get; set; }
+		public string Title { get; set; } = null!;
+		public string ImageUrl { get; set; } = null!;
+		public string Content { get; set; } = null!;
+		public string PublishDate { get; set; } = null!;
+		public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+	}
+}
