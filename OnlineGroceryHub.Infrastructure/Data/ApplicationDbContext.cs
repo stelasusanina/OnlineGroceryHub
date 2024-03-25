928daec807c781ca735601b485c4e309c9dfa833
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-03-25 12:31:11 +0200

    Dbsets added

--- OnlineGroceryHub.Infrastructure/Data/ApplicationDbContext.cs
+++ OnlineGroceryHub.Infrastructure/Data/ApplicationDbContext.cs
@@ -7,39 +7,41 @@ using System.Reflection.Emit;
 
 namespace OnlineGroceryHub.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
 
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<SubCategory> SubCategories { get; set; }
-        public DbSet<Article> Articles { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-        public DbSet<ArticleComment> ArticlesComments { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<SubCategory> SubCategories { get; set; }
+		public DbSet<Article> Articles { get; set; }
+		public DbSet<Comment> Comments { get; set; }
+		public DbSet<ArticleComment> ArticlesComments { get; set; }
+		public DbSet<Wishlist> Wishlists { get; set; }
+		public DbSet<WishlistProduct> WishlistsProducts { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.ApplyConfiguration(new CategoryConfiguration());
-            builder.ApplyConfiguration(new SubCategoryConfiguration());
-            builder.ApplyConfiguration(new ProductConfiguration());
-            builder.ApplyConfiguration(new ArticleConfiguration());
-            builder.ApplyConfiguration(new CommentConfiguration());
-            builder.ApplyConfiguration(new ArticleCommentConfiguration());
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.ApplyConfiguration(new CategoryConfiguration());
+			builder.ApplyConfiguration(new SubCategoryConfiguration());
+			builder.ApplyConfiguration(new ProductConfiguration());
+			builder.ApplyConfiguration(new ArticleConfiguration());
+			builder.ApplyConfiguration(new CommentConfiguration());
+			builder.ApplyConfiguration(new ArticleCommentConfiguration());
 
-            builder.Entity<Product>()
-                .Property(p => p.Price)
-                .HasPrecision(18, 2);
+			builder.Entity<Product>()
+				.Property(p => p.Price)
+				.HasPrecision(18, 2);
 
-            builder.Entity<ArticleComment>()
-                .HasKey(ac => new
-                {
-                    ac.ArticleId,
-                    ac.CommentId
-                });
+			builder.Entity<ArticleComment>()
+				.HasKey(ac => new
+				{
+					ac.ArticleId,
+					ac.CommentId
+				});
 
 			builder.Entity<ArticleComment>()
 			.HasOne(ac => ac.Article)
@@ -51,7 +53,24 @@ namespace OnlineGroceryHub.Data
 				.WithMany(c => c.ArticleComments)
 				.HasForeignKey(ac => ac.CommentId);
 
+			builder.Entity<WishlistProduct>()
+				.HasKey(wp => new
+				{
+					wp.WishlistId,
+					wp.ProductId
+				});
+
+			builder.Entity<WishlistProduct>()
+				.HasOne(wp => wp.Wishlist)
+				.WithMany(w => w.WishlistProducts)
+				.HasForeignKey(wp => wp.WishlistId);
+
+			builder.Entity<WishlistProduct>()
+				.HasOne(wp => wp.Product)
+				.WithMany()
+				.HasForeignKey(p => p.ProductId);
+
 			base.OnModelCreating(builder);
-        }
-    }
+		}
+	}
 }
